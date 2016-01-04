2d7370394a301fabc7320726e1282106fcdd34ce
David Fuka <gravetotkd@gmail.com>
2016-01-03 21:28:20 -0700

    Initial commit
    Initial commit to Pluralsight repo

---
+++ NodeExpress4/app.js
@@ -0,0 +1,61 @@
+(function () {
+    'use strict';
+
+    var express = require('express'),
+        handlebars = require('express-handlebars'),
+        app = express(),
+        port = process.env.port || 5000,
+        bookRouter = express.Router(),
+        books = [{
+            name: 'Something'
+        }];
+
+    app.use(express.static('public'));
+    app.set('views', 'src/views');
+
+    // use the following code when using handlebars
+    //    app.engine('.hbs', handlebars({
+    //        extname: '.hbs'
+    //    }));
+
+    app.set('view engine', '.ejs'); //for jade use .jade; for handlebars use .hbs
+
+    bookRouter.route('/')
+        .get(function (req, res) {
+            res.render('books', {
+                title: 'Hello from render',
+                nav: [{
+                    Link: '/books',
+                    Text: 'Books'
+                }, {
+                    Link: '/authors',
+                    Text: 'Authors'
+                }],
+                books: books
+            });
+        });
+
+    bookRouter.route('/single')
+        .get(function (req, res) {
+            res.send('Hello single book');
+        });
+
+    app.use('/books', bookRouter);
+
+    app.get('/', function (req, res) {
+        res.render('index', {
+            title: 'Hello from render',
+            nav: [{
+                Link: '/books',
+                Text: 'Books'
+            }, {
+                Link: '/authors',
+                Text: 'Authors'
+            }]
+        });
+    });
+
+    app.listen(port, function (err) {
+        console.log('running server on port - ' + port);
+    });
+}());
